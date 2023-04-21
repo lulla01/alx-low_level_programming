000d49938bcfeb628672b1d4c87cc6c9e10e24f2
Master Bee <lullaluve@gmail.com>
2023-04-21 08:13:03 -0700

    Revision of Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,9 @@
  */
 int main(void)
 {	
-	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	char str[] = "Programming is like building a multilingual puzzle";
+	
+	puts(str);
+	
+	return (0);
 }
