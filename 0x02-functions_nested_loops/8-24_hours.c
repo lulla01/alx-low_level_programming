00ed65911fdce7ab6145d124271f24bf0de18b7a
Master Bee <lullaluve@gmail.com>
2023-04-25 18:18:46 -0700

    Jack Bauer in functions and nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,7 +15,6 @@ void jack_bauer(void)
 
 	h2 = 0;
 	while (h2 <= 2)
-
 	{
 		if (h2 == 2)
 		{
@@ -24,15 +23,12 @@ void jack_bauer(void)
 		}
 		h1 = 0;
 		while (h1 <= a)
-
 		{
 			m2 = 0;
-			while (m2 <= 5)
-			
+			while (m2 <= 5)			
 			{
 				m1 = 0;
 				while (m1 <= 9)
-
 				{
 					_putchar('0' + h2);
 					_putchar('0' + h1);
